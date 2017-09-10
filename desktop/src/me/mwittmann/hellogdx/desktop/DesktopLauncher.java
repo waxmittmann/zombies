484c9e1a6cf71c5a3e5c7d1774369e23ae969522
Max Wittmann <damxam@gmail.com>
2017-09-10 12:25:13 +1000

    Initial commit

---
+++ desktop/src/me/mwittmann/hellogdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.mwittmann.hellogdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.mwittmann.hellogdx.HelloGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloGdx(), config);
+	}
+}
